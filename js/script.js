9bf84ed791264fe1cbc72d84996296688b58285d
Philip Thomson <pst16@georgetown.edu>
2024-01-07 21:29:08 -0500

    Added validation rules and linked to script.js
    Linked index.html to script.js
    Added validation rules to script.js and style.css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 /* script.js */
+
+const passInput = document.getElementById("password");
+const confirmPassInput = document.getElementById("password-confirm");
+
+addInputListeners();
+
+function addInputListeners() {
+
+    passInput.addEventListener("input", (event) => {
+
+        validatePassword();
+    });
+
+    confirmPassInput.addEventListener("input", (event) => {
+
+        validatePassword();
+    });
+}
+
+function validatePassword() {
+
+    let pass = passInput.value;
+    let confirm = confirmPassInput.value;
+
+    if(pass != confirm) {
+
+        confirmPassInput.setCustomValidity("Hello");
+    }
+    else {
+
+        confirmPassInput.setCustomValidity(""); // Validates
+    }
+}
